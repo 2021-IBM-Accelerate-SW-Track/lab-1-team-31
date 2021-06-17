f6c010155087d6cd8c4a8a21cfcbf59827859ff0
Byung Moon Kim <kimbm2014@gmail.com>
2021-06-17 09:07:01 -0400

    updated header's css

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,8 +1,12 @@
+import './header.css';
+
 export default function Header(){
     return (
         <div>
+        <header>
             2021 IBM Accelerate Software Track Lab Boiler Plate
-            <header><h1>GROUP 31's TO-DO LIST</h1></header>
+            <h1>GROUP 31's TO-DO LIST</h1>
+        </header>
         </div>
     )
 
