e0f491fdef5b99e15376ec800ceb182536e78b3d
xinyiluo448 <65972761+xinyiluo448@users.noreply.github.com>
2021-06-19 04:21:52 +0800

    TodoList App with adding, deleting, marking as complete, date and time

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import Header from "./component/header"
+import React, {useState} from "react";
 import './App.css';
+import DenseAppBar from "./appBar";
+import TodoForm from './component/todo/todoForm';
+import TodoList from './component/todo/todoList';
+import { v4 as uuid } from 'uuid';
+
+function buttonHandler() {
+  alert('clicked')
+}
 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
-  );
-}
+  var today = new Date();
+  const [note, setNote] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: uuid(),
+      title: text,
+      content: "",
+      completed: false,
+      date: (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear(),
+      time: today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+    }
+    setNote([...note, newTodo]);
+  }
+
+    const checkTodo = (id) => {
+      setNote(note.map(task => {
+        if (id === task.id) task.completed = !task.completed;
+        return task;
+      }))
+    }
+
+    const deleteTodo = (id) => {
+      setNote(note.filter(todo => todo.id !== id))
+    }
+
+		return (
+			<div className="App">
+				<DenseAppBar />
+				<br />
+				<TodoForm addTodo = {addTodo}/>
+				<TodoList note = {note} checkTodo = {checkTodo} deleteTodo = {deleteTodo}/>
+			</div>
+		);
+};
+
+
 
 export default App;
