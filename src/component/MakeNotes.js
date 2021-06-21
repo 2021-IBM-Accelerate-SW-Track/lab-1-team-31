80222931732cbceaa21dc0090aa295a51bd26e93
Hussein Fuaad <47349274+fuaad001@users.noreply.github.com>
2021-06-22 00:58:21 +0300

    Update MakeNotes.js

--- src/component/MakeNotes.js
+++ src/component/MakeNotes.js
@@ -2,17 +2,20 @@ import React , {useState} from "react";
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
+import {FormControl, Container, TextField, Button} from '@material-ui/core'
 
 
 function MakeNotes(props){
 
     const [isExpanded, setExpanded] = useState(false);
+    var today = new Date();
 
     //initial values 
-
     const [note, setNote] = useState({
-        title : "",
-        content: ""
+        content: "",
+        completed: false,
+        time: today.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}),
+        date: today.toLocaleDateString()
     });
 
 
@@ -28,14 +31,18 @@ function MakeNotes(props){
             };
         });
     }
+
     function submitNote(event){
         props.onAdd(note);
 
 
         //after click "add" button, remove typed messages
         setNote({
-            title: "",
-            content: ""
+            //title: "",
+            content: "",
+            completed: false,
+            time: today.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}),
+            date: today.toLocaleDateString()
         })
 
         event.preventDefault(); //to prevent refreshing 
@@ -46,27 +53,28 @@ function MakeNotes(props){
       }
     return (
         <div>
-            <form className="create-note">
-            {isExpanded && (
-                <input 
-                    onChange={handleChange}
-                    name="title"
-                    value={note.title}
-                    placeholder="Title"
-                />
-            )}
-                <textarea 
-                    onClick={expand}
-                    onChange={handleChange}
-                    name="content"
-                    value={note.content}
-                    placeholder="Take a note.."
-                    row={isExpanded ? 3 : 1}
-                />
-                <Zoom in={isExpanded}>
-                    <Fab onClick={submitNote}><AddIcon /></Fab> 
-                </Zoom>
-            </form>
+			<form className="create-note" onSubmit = {submitNote}>
+				<FormControl fullWidth = {true}>
+					<TextField 
+                        required = {true}
+                        onClick={expand}
+                        onChange={handleChange}
+                        name="content"
+                        value={note.content}
+                        placeholder="Create task..."
+                        row={isExpanded ? 2 : 1}
+                        InputProps={{ disableUnderline: true }}
+                        data-testid = "new-item-input"
+                    />
+					<Button 
+                        variant = "contained" 
+                        type = "submit"
+                        style = {{marginTop: 5}} 
+                        data-testid = "new-item-button">
+						<AddIcon/>
+					</Button>
+				</FormControl>
+			</form>
         </div>
     );
 }
